c9e5ff5e4862a25201c5bffedc11d29d404460d1
Marc Hervera <mhergon@gmail.com>
2017-11-01 13:29:59 +0100

    * Fix header file

---
+++ Eaglebit/EagleBit.h
@@ -0,0 +1,19 @@
+//
+//  EagleBit.h
+//  EagleBit
+//
+//  Created by mhergon on 1/11/17.
+//  Copyright © 2017 mhergon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Eaglebit.
+FOUNDATION_EXPORT double EagleBitVersionNumber;
+
+//! Project version string for Eaglebit.
+FOUNDATION_EXPORT const unsigned char EagleBitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EagleBit/PublicHeader.h>
+
+
